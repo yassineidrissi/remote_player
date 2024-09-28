334d4f881730e5e50920e6ac1aeaf4f6217832ce
yassine idrissi <yassine42idrissi@gmail.com>
2024-09-28 21:34:25 +0100

    add two html files for single page and remote players

--- app/routing.py
+++ app/routing.py
@@ -5,6 +5,7 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/rooms/$', RoomConsumer.as_asgi()),
     path('ws/game/', consumers.GameRoomConsumer.as_asgi()),
+    # path('ws/singlegame/', consumers.SingleGameConsumer.as_asgi()),
     # path('tournament/<str:room_name>/', TournamentConsumer.as_asgi()),
     # path('tournament/', RoomConsumer.as_asgi()),
 ]
